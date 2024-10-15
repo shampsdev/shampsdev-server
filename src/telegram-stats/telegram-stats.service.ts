ea7b31b963331e861fb20a6e1587e9c1c6cf167d
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-16 02:06:43 +0300

    updated some small things

--- src/telegram-stats/telegram-stats.service.ts
+++ src/telegram-stats/telegram-stats.service.ts
@@ -23,7 +23,7 @@ export class TelegramStatsService {
 
     await this.statsService.createStat({
       stat_id: 'telegram_channel_count',
-      name: 'подписчиков в телеграм канале',
+      name: 'в телеграм канале',
       count: updatedCount,
     });
 
@@ -36,38 +36,25 @@ export class TelegramStatsService {
     }
   }
 
-  @On('message')
-  async onMessage(ctx: Context) {
-    if (ctx.message.chat.type !== 'private') return;
-
-    const stat_id = ctx.text.split(' ')[0];
-    const count = parseInt(ctx.text.split(' ')[1]);
-    const name = ctx.text.split(' ').slice(2).join(' ');
-
-    await this.statsService.createStat({
-      stat_id,
-      count,
-      name,
-    });
-  }
-
   @On('text')
   async onComment(ctx: Context) {
-    const lastMessage = ctx.message;
-
-    await this.statsService.createStat({
-      stat_id: 'telegram_channel_commented',
-      name: `${lastMessage.from.first_name} прокоментировал "${ctx.text}"`,
-      count: 0,
-    });
-  }
-
-  @On('boost_added')
-  async onBoost(ctx: Context) {
-    await this.statsService.createStat({
-      stat_id: 'telegram_channel_boost',
-      name: `${ctx.chatBoost.boost.source.user.first_name} кинул буст`,
-      count: 0,
-    });
+    if (ctx.chat.type === 'private') {
+      console.info('updated stat');
+      const stat_id = ctx.text.split(' ')[0];
+      const count = parseInt(ctx.text.split(' ')[1]);
+      const name = ctx.text.split(' ').slice(2).join(' ');
+      await this.statsService.createStat({
+        stat_id,
+        count,
+        name,
+      });
+    } else {
+      console.info('added comment');
+      await this.statsService.createStat({
+        stat_id: 'telegram_channel_commented',
+        name: `"${ctx.text}"`,
+        count: 0,
+      });
+    }
   }
 }
